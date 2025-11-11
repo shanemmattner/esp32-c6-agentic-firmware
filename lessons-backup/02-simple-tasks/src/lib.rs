89f28b71f53408e616d4c50e6ba41734a55efc09
Shane Mattner <shanemattner@Shanes-MacBook-Pro.local>
2025-11-10 19:59:33 -0800

    remove backups

--- lessons-backup/02-simple-tasks/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Lesson 02: Simple Task Scheduler
-//!
-//! Library exports for the task scheduler and tasks.
-
-#![no_std]
-
-pub mod scheduler;
-pub mod tasks;
--- lessons-backup/03-mpu9250/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Lesson 03: MPU9250 IMU Sensor
-//!
-//! Library exports for MPU9250 sensor reading with task scheduler.
-
-#![no_std]
-
-pub mod mpu9250;
-pub mod scheduler;
-pub mod tasks;
